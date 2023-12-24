6148186e816f6e1c04e7fd0fc43eb617bd76c9f1
LeonYu <1099052496@qq.com>
2023-12-25 00:11:17 +0800

    test: try

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,13 +10,13 @@ module.exports = {
     'plugin:prettier/recommended',
     'eslint-config-prettier'
   ],
-  // parseOptions: {
-  //   ecmaVersion: 'latest',
-  //   sourceType: 'module',
-  //   ecmaFeatures: {
-  //     jsx: true
-  //   }
-  // },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   settings: {
     react: {
       version: 'detect'
@@ -27,43 +27,43 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'react-refresh', 'import', 'prettier'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    // 'import/order': [
-    //   'error',
-    //   {
-    //     // 按照分组顺序进行排列
-    //     groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'internal', 'object', 'type'],
-    //     pathGroups: [
-    //       {
-    //         pattern: 'react*', // 对含 react 的包进行匹配
-    //         group: 'builtin', // 将其定义为builtin模块
-    //         position: 'before' // 定义在 builtin 模块中的优先级
-    //       },
-    //       {
-    //         pattern: '@/components/**',
-    //         group: 'parent',
-    //         position: 'before'
-    //       },
-    //       {
-    //         pattern: '@/utils/**',
-    //         group: 'parent',
-    //         position: 'after'
-    //       },
-    //       {
-    //         pattern: '@/apis/**',
-    //         group: 'parent',
-    //         position: 'after'
-    //       }
-    //     ],
-    //     // 将 react 包不进行排序，并放在前排，可以保证react包放在第一排
-    //     pathGroupsExcludeImportTypes: ['react'],
-    //     'newlines-between': 'always', // 每个分组之间进行换行
-    //     // 根据字母顺序对每个组内的顺序进行排序
-    //     alphabetize: {
-    //       order: 'asc',
-    //       caseInsensitive: true
-    //     }
-    //   }
-    // ],
+    'import/order': [
+      'error',
+      {
+        // 按照分组顺序进行排列
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'internal', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react*', // 对含 react 的包进行匹配
+            group: 'builtin', // 将其定义为builtin模块
+            position: 'before' // 定义在 builtin 模块中的优先级
+          },
+          {
+            pattern: '@/components/**',
+            group: 'parent',
+            position: 'before'
+          },
+          {
+            pattern: '@/utils/**',
+            group: 'parent',
+            position: 'after'
+          },
+          {
+            pattern: '@/apis/**',
+            group: 'parent',
+            position: 'after'
+          }
+        ],
+        // 将 react 包不进行排序，并放在前排，可以保证react包放在第一排
+        pathGroupsExcludeImportTypes: ['react'],
+        'newlines-between': 'always', // 每个分组之间进行换行
+        // 根据字母顺序对每个组内的顺序进行排序
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ],
     '@typescript-eslint/no-explicit-any': ['off'], // 允许使用 any
     '@typescript-eslint/no-non-null-assertion': 'off', // 允许使用非空断言
     'no-console': [
--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
 };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+}
