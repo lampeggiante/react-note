1515aab0f9b862f29b52130c59fa4ece3b40c58d
LeonYu <1099052496@qq.com>
2023-12-29 23:29:55 +0800

    fix: dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-note/",
   resolve: {
     alias: {
       "@": path.resolve("./src"),
